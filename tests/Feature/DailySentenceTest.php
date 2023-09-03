bfe6ee85c90f6b135dbda318dabecde8ee6d6a1f
monkeyteacher <peter1180041@gmail.com>
2023-09-03 15:11:05 +0800

    新增GetDataFromItsthisforthat測試

--- tests/Feature/DailySentenceTest.php
+++ tests/Feature/DailySentenceTest.php
@@ -3,16 +3,12 @@
 namespace Tests\Feature;
 
 use App\Services\GetDataFromMetaphorpsum;
+use App\Services\GetDataFromItsthisforthat;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class DailySentenceTest extends TestCase
 {
-    /**
-    * A basic feature test example.
-    *
-    * @return void
-    */
     public function test_GetDataFromMetaphorpsum_is_work()
     {
         Http::fake([
@@ -24,6 +20,21 @@ class DailySentenceTest extends TestCase
         $expectedValues = [
             'status'=>'success',
             'data'=>'Hello metaphorpsum'
+        ];
+        $this->assertEquals($expectedValues, $result);
+    }
+
+    public function test_GetDataFromItsthisforthat_is_work()
+    {
+        Http::fake([
+            'https://itsthisforthat.com/api.php?text' => Http::response('Hello itsthisforthat', 200),
+        ]);
+        $getDataFromItsthisforthat = new GetDataFromItsthisforthat();
+        $result = $getDataFromItsthisforthat->getSentence();
+        $this->assertIsArray($result);
+        $expectedValues = [
+            'status'=>'success',
+            'data'=>'Hello itsthisforthat',
         ];
         $this->assertEquals($expectedValues, $result);
     }
